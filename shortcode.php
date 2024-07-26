f9751a93e5b07ccffabb45773fa22fdf84214b25
J. Rotich <135043203+amjrotich@users.noreply.github.com>
2024-07-27 02:27:37 +0300

    Update shortcode.php

--- shortcode.php
+++ shortcode.php
@@ -1,22 +1,23 @@
+<?php
 // Add Shortcode
 function subform() {
 
     // Get the current user's display name
     $current_user = wp_get_current_user();
-    $user_display_name = $current_user->user_firstname;
+    $user_display_name = $current_user->display_name;
 
     // Get the blog name
-    $blog_name = get_bloginfo( 'name' );
+    $blog_name = get_bloginfo('name');
 
-    // Build the output
-    $output = '<p>Hey ' . $user_display_name . ', welcome to ' . $blog_name . '! You can subscribe to our newsletter here:</p>';
-    $output .= '<form action="/thank-you">';
-    $output .= '<label for="email">Enter your email:</label>';
-    $output .= '<input type="email" id="email" name="email">';
-    $output .= '<input type="submit" value="Submit">';
-    $output .= '</form>';
-
-    // Return the output
-    return $output;
+    // Display the welcome message and subscription form
+    echo '<p>Hey ' . esc_html($user_display_name) . ', welcome to ' . esc_html($blog_name) . '! You can subscribe to our newsletter here:</p>';
+    ?>
+    <form action="/thank-you">
+        <label for="email">Enter your email:</label>
+        <input type="email" id="email" name="email">
+        <input type="submit" value="Submit">
+    </form>
+    <?php
 }
-add_shortcode( 'subscriptionform', 'subform' );
+add_shortcode('subscriptionform', 'subform');
+?>
